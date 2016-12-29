21d5d5c801bf156fb3f5360299c48fb19e84a1d4
emanuelzh <emanuelz@emanuelz.com.mx>
2016-12-29 02:59:58 -0600

    Add software provisioning file

---
+++ provision.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#initial update and basics (mostly already installed by default)
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install -y language-pack-en-base
+sudo apt-get install -y software-properties-common
+sudo apt-get install -y curl vim git wget
+sudo apt-get install -y sqlite3
+
+#MySQL (5.7 default in Ubuntu 16)
+sudo apt install -y mysql-server mysql-client
+
+#PHP 7.0
+sudo apt-get install -y php7.0
+sudo apt-get install -y php7.0-mysql
+php -v
+
+#composer
+sudo curl -sS https://getcomposer.org/installer | php
+sudo mv composer.phar /usr/local/bin/composer
+composer --version
