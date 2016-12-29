9f449d03ddd0f1368359521a18c0f84bfe4e7294
Emanuel Zámano <emanuel.zamano@gmail.com>
2016-12-29 11:54:24 -0600

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -6,13 +6,14 @@ sudo apt-get install -y language-pack-en-base
 sudo apt-get install -y software-properties-common
 sudo apt-get install -y curl vim git wget
 sudo apt-get install -y sqlite3
+sudo apt-get install -y zip unzip
 
 #MySQL (5.7 default in Ubuntu 16)
 sudo apt install -y mysql-server mysql-client
 
 #PHP 7.0
 sudo apt-get install -y php7.0
-sudo apt-get install -y php7.0-mysql
+sudo apt-get install -y php7.0-mysql php7.0-mbstring
 php -v
 
 #composer
